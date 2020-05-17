8e18fb8fad565ad83369053918e88877e844b52d
Oliver Friedmann <oliver.friedmann@gmail.com>
2020-05-16 23:32:43 -0400

    Metric parsing vnstat

--- src/vnstat.js
+++ src/vnstat.js
@@ -3,6 +3,22 @@ Scoped.require([
     "betajs:Promise"
 ], function (Promise) {
 
+    var parseMetric = function (metric) {
+        var metrics = {
+            "B": 0.001 * 0.001,
+            "KiB": 0.001,
+            "MiB": 1,
+            "GiB": 1000,
+            "TiB": 1000 * 1000
+        };
+        return metrics[metric] || 0;
+    };
+
+    var parseTraffic = function (traffic) {
+        traffic = (traffic || "").match(/^([^\s]+)\s([^\s]+)/);
+        return traffic ? parseFloat(traffic[1]) * parseMetric(traffic[2]) : null;
+    };
+
     var parseVnstat = function (input) {
         var lines = input.trim().split("\n");
         var output = {};
@@ -20,10 +36,10 @@ Scoped.require([
                         break;
                     }
                     result[entry[1]] = {
-                        rx: entry[2],
-                        tx: entry[3],
-                        total: entry[4],
-                        estimated: entry[5]
+                        rx: parseTraffic(entry[2]),
+                        tx: parseTraffic(entry[3]),
+                        total: parseTraffic(entry[4]),
+                        estimated: parseTraffic(entry[5])
                     };
                 }
                 output[base[1]] = result;
@@ -37,13 +53,20 @@ Scoped.require([
 
         vnstat: function () {
             var promise = Promise.create();
-            require("child_process").exec("sudo vnstat", function (error, stdout, stderr) {
-                if (error) {
-                    promise.asyncError({
-                        result: "UnknownError",
-                        error: error,
-                        stdout: stdout,
-                        stderr: stderr
+            var CP = require("child_process");
+            CP.exec("sudo vnstat -s", function (error, stdout, stderr) {
+                if (error || (stdout + "").indexOf("Error") >= 0) {
+                    CP.exec("sudo vnstat", function (error, stdout, stderr) {
+                        if (error) {
+                            promise.asyncError({
+                                result: "UnknownError",
+                                error: error,
+                                stdout: stdout,
+                                stderr: stderr
+                            });
+                            return;
+                        }
+                        promise.asyncSuccess(parseVnstat(stdout));
                     });
                     return;
                 }
